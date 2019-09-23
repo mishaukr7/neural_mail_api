d15748921e3ef479b7b8fc135af87a7fde8475ce
mykhailo <m.o.khomenko@gmail.com>
2019-09-23 18:06:51 +0300

    Added apps

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
 from .config import app_config
 
 db = SQLAlchemy()
+flask_bcrypt = Bcrypt()
 
 
 def create_app(env_name):
@@ -11,7 +13,6 @@ def create_app(env_name):
     Create app
     """
 
-    # app initiliazation
     app = Flask(__name__)
 
     app.config.from_object(app_config[env_name])
---
+++ api/utils/abstract.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from ..app import db
+
+
+class TimeStampedModel(db.Model):
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __init__(self):
+        self.created_at = datetime.utcnow()
+        self.modified_at = datetime.utcnow()
